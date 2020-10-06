a1a0f877fb563b907dde1c41126df3311db96594
Jeremias Pettinen <hi@jeremi.as>
2020-10-06 22:30:37 +0000

    Start implementing Twitter control commands

--- src/modules/twitter.ts
+++ src/modules/twitter.ts
@@ -1,18 +1,8 @@
 import fetch from 'node-fetch'
-
-import { logSprintf } from '../globals'
-import { EventEmitter } from 'events'
 import Commando = require( 'discord.js-commando' )
-import { Channel, Client, ClientOptions, Collection, DMChannel, Emoji, Guild, GuildChannel, GuildMember, GuildResolvable, Message, MessageAttachment, MessageEmbed, MessageMentions, MessageOptions, MessageAdditions, MessageReaction, PermissionResolvable, PermissionString, ReactionEmoji, Role, Snowflake, StringResolvable, TextChannel, User, UserResolvable, VoiceState, Webhook } from 'discord.js'
+import { Message, TextChannel } from 'discord.js'
 
-import sprintfjs = require( 'sprintf-js' )
-const sprintf = sprintfjs.sprintf
-
-import { Backend } from '../lib/backend'
-import { Parser } from '../lib/parser'
-import { Redis } from '../lib/redis'
-
-import { CommandCallbackType, NyaInterface, ModuleBase } from '../modules/module'
+import { NyaInterface, ModuleBase } from '../modules/module'
 
 class TwitterChannelCommand extends Commando.Command
 {
@@ -25,31 +15,127 @@ class TwitterChannelCommand extends Commando.Command
       name: 'twitterchannel',
       group: 'twitter',
       memberName: 'twitterchannel',
-      description: 'Set a channel for posting tweets.',
+      description: "Set a channel for posting tweets.",
       args: [{
         key: 'target',
         prompt: "Which channel?",
-        type: 'channel'
+        type: 'channel',
+        default: ''
       }],
       argsPromptLimit: 1
     })
     this._service = service
   }
 
-  async run( message: Commando.CommandoMessage, args: object | string | string[], fromPattern: boolean, result?: Commando.ArgumentCollectorResult ): Promise<Message | Message[] | null>
+  async run( message: Commando.CommandoMessage, args: any, fromPattern: boolean, result?: Commando.ArgumentCollectorResult ): Promise<Message | Message[] | null>
   {
-    console.log("!twitterchannel", message, args)
+    const host = this._service.getHost()
 
-    if ( false /* TODO: if can't post to channel etc. */ )
-      return this._service.getHost().respondTo( message, 'twitterchannel_fail' )
-    return this._service.getHost().respondTo( message, 'twitterchannel_set' )
+    if ( !args.target ) {
+      // TODO: show currently set channel
+      if ( false ) // TODO: if unset
+        return host.respondTo( message, 'twitterchannel_unset' )
+      return host.respondTo( message, 'twitterchannel_show', '738183830069837907' )
+    }
+
+    console.log("!twitterchannel", args)
+    if ( false ) // TODO: if channel is not a text channel or can't be posted to
+      return host.respondTo( message, 'twitterchannel_fail' )
+    return host.respondTo( message, 'twitterchannel_set', '738183830069837907' )
   }
 }
 
+class TwitterAddCommand extends Commando.Command
+{
+  protected _service: ModuleBase
+
+  constructor( service: ModuleBase, client: Commando.CommandoClient )
+  {
+    // TODO: allow following multiple Twitter handles at once?
+    super( client,
+    {
+      name: 'twitteradd',
+      group: 'twitter',
+      memberName: 'twitteradd',
+      description: "Add a Twitter account to follow.",
+      args: [{
+        key: 'username',
+        prompt: "Which Twitter account?",
+        type: 'string'
+      }],
+      argsPromptLimit: 1
+    } )
+    this._service = service
+  }
+
+  async run( message: Commando.CommandoMessage, args: any, fromPattern: boolean, result?: Commando.ArgumentCollectorResult ): Promise<Message | Message[] | null>
+  {
+    const host = this._service.getHost()
+    console.log("!twitteradd", args)
+    if ( false ) // TODO: if account is already being followed
+      return host.respondTo( message, 'twitteradd_already_following', 'username' )
+
+    let username = args.username
+    if ( username.startsWith( '@' ) )
+      username = username.substr( 1 )
+    if ( !/^\w+$/.test( username ) )
+      return host.respondTo( message, 'twitteradd_nonexistent', username )
+
+    const config = this._service.getBackend()._config.twitter
+    const fetchOpts = {
+      headers: {
+        Authorization: `Bearer ${config.bearerToken}`
+      }
+    }
+    const accountExists = await fetch( `https://api.twitter.com/2/users/by/username/${username}`, fetchOpts )
+      .then( response => response.json() )
+      .then( response => !!response.data )
+
+    if ( !accountExists )
+      return host.respondTo( message, 'twitteradd_nonexistent', username )
+
+    // TODO: actually commit changes to DB
+    return host.respondTo( message, 'twitteradd_success', username )
+  }
+}
+
+class TwitterDeleteCommand extends Commando.Command
+{
+  protected _service: ModuleBase
+
+  constructor( service: ModuleBase, client: Commando.CommandoClient )
+  {
+    // TODO: allow unfollowing multiple Twitter handles at once?
+    super( client,
+    {
+      name: 'twitterdel',
+      group: 'twitter',
+      memberName: 'twitterdel',
+      description: "Stop following a Twitter account.",
+      args: [{
+        key: 'handle',
+        prompt: "Which Twitter account?",
+        type: 'string'
+      }],
+      argsPromptLimit: 1
+    } )
+    this._service = service
+  }
+
+  async run( message: Commando.CommandoMessage, args: object | string | string[], fromPattern: boolean, result?: Commando.ArgumentCollectorResult ): Promise<Message | Message[] |
+null>
+  {
+    const host = this._service.getHost()
+    console.log("!twitterdel", args)
+    if ( false ) // TODO: if account wasn't being followed
+      return host.respondTo( message, 'twitterdel_notfollowing' )
+    return host.respondTo( message, 'twitterdel' )
+  }
+}
+
+
 export class TwitterModule extends ModuleBase
 {
-  _parser: Parser
-
   constructor( id: number, host: NyaInterface, client: Commando.CommandoClient )
   {
     super( id, host, client )
@@ -63,10 +149,17 @@ export class TwitterModule extends ModuleBase
     const redis = this._backend._redis
 
     for ( const guildID of [1] ) { // TODO
+      // TODO: get channel from DB
+      let channel = client.channels.cache.get( '738183830069837907' )
+      if ( !( channel instanceof TextChannel ) )
+        continue
+
       const redisKey = `latesttweet_${guildID}`
 
       setInterval( async function() {
-        const query = 'from:ahogasm'
+        // TODO: get accounts from DB
+        const accounts = ['ahogeez', 'ahogasm', 'neonyaparty', 'neonyastream']
+        const query = accounts.map( (handle: string) => `from:${handle}` ).join(' OR ')
 
         const latestTweet = await redis.get( redisKey )
         const since = latestTweet ? `&since_id=${latestTweet}` : ''
@@ -95,11 +188,8 @@ export class TwitterModule extends ModuleBase
               const user = users.find( (user: any) => user.id === tweet.author_id )
               const username = user ? user.name : tweet.author_id
               const handle = user ? user.username : 'i'
-              const message = `**${username}** tweeted: https://twitter.com/${handle}/status/${tweet.id}`
-              console.log('CHANNEL', client.channels.cache.get('738183830069837907'))
-              const channel = client.channels.cache.get( '738183830069837907' )
-              if ( channel )
-                (channel as TextChannel).send( message )
+              const message: string = `**${username}** tweeted: https://twitter.com/${handle}/status/${tweet.id}`;
+              ( channel as TextChannel ).send( message )
             }
           } )
         } )
@@ -121,7 +211,9 @@ export class TwitterModule extends ModuleBase
   getCommands(): Commando.Command[]
   {
     return [
-      new TwitterChannelCommand( this, this.getClient() )
+      new TwitterChannelCommand( this, this.getClient() ),
+      new TwitterAddCommand( this, this.getClient() ),
+      new TwitterDeleteCommand( this, this.getClient() )
     ]
   }
 
