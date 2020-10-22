01f30b7bd6814c41d6b115f0932e1b26a017bac9
Jeremias Pettinen <hi@jeremi.as>
2020-10-22 15:56:57 +0000

    Twitch: improve error handling and logging

--- src/modules/twitch.ts
+++ src/modules/twitch.ts
@@ -1,6 +1,6 @@
 import Commando = require( 'discord.js-commando' )
 import { Message, MessageEmbed, TextChannel } from 'discord.js'
-import { ApiClient, HelixStream, HelixStreamType } from 'twitch'
+import { ApiClient, HelixStream, HelixStreamType, HelixUser } from 'twitch'
 import { ClientCredentialsAuthProvider } from 'twitch-auth'
 import { ReverseProxyAdapter, Subscription, WebHookListener } from 'twitch-webhooks'
 
@@ -44,11 +44,23 @@ class TwitchChannelCommand extends Commando.Command
     const guild = await backend.getGuildBySnowflake( message.guild.id )
 
     if ( !args.channel ) {
-      const setting = await backend.getGuildSetting( guild.id, settingKey )
+      let setting
+      try {
+        setting = await backend.getGuildSetting( guild.id, settingKey )
+      } catch ( error ) {
+        log( `Failed to fetch Twitch channel setting for guild ${guild.id}:`, error )
+        return host.respondTo( message, 'unexpected' )
+      }
       if ( !setting || !setting.value )
         return host.respondTo( message, 'twitchchannel_unset' )
 
-      const channel = await client.channels.fetch( setting.value )
+      let channel
+      try {
+        channel = await client.channels.fetch( setting.value )
+      } catch ( error ) { 
+        log( `Failed to fetch channel ${setting.value}:`, error )
+        return host.respondTo( message, 'unexpected' )
+      }
       if ( !channel || channel.type !== 'text' )
         return host.respondTo( message, 'twitchchannel_unset' )
 
@@ -59,8 +71,13 @@ class TwitchChannelCommand extends Commando.Command
     if ( channel.type !== 'text' )
       return host.respondTo( message, 'twitchchannel_fail' )
 
+    try {
+      await backend.setGuildSetting( guild.id, settingKey, channel.id )
+    } catch ( error ) {
+      log( `Failed to set Twitch channel to ${channel.id} in guild ${guild.id}:`, error )
+      return host.respondTo( message, 'unexpected' )
+    }
     this._service.channels.set( guild.id, channel.id )
-    await backend.setGuildSetting( guild.id, settingKey, channel.id )
     return host.respondTo( message, 'twitchchannel_set', channel.id )
   }
 }
@@ -104,22 +121,39 @@ class TwitchImageCommand extends Commando.Command
     const settingKey = this._service.settingKeys.images
     const host = this._service.getHost()
     const backend = host.getBackend()
-    const guild = await backend.getGuildBySnowflake( message.guild.id )
+
+    let guild
+    try {
+      guild = await backend.getGuildBySnowflake( message.guild.id )
+    } catch ( error ) {
+      log( `Failed to fetch guild ${message.guild.id}:`, error )
+      return host.respondTo( message, 'unexpected' )
+    }
+
+    let setting
+    try {
+      setting = await backend.getGuildSetting( guild.id, settingKey )
+    } catch ( error ) {
+      log( `Failed to fetch ${settingKey} setting for guild ${guild.id}:`, error )
+      setting = null
+    }
 
     let images
-    const setting = await backend.getGuildSetting( guild.id, settingKey )
-
     if ( setting && setting.value ) {
       try {
         images = JSON.parse( setting.value )
-        if ( typeof images !== 'object' ) {
-          const message = `${settingKey} must be a JSON object.`
-          log( message )
-          throw new Error( message )
-        }
-      } catch ( error ) {
+        if ( typeof images !== 'object' )
+          throw new Error( `The ${settingKey} setting must be a JSON object.` )
+      } catch ( _ ) {
+        log( `The ${settingKey} setting of guild ${guild.id} was not a JSON object.` )
         images = {}
-        await backend.setGuildSetting( guild.id, settingKey, JSON.stringify( images ) )
+        const jsonString = JSON.stringify( images )
+        try {
+          await backend.setGuildSetting( guild.id, settingKey, jsonString )
+        } catch ( error ) {
+          log( `Failed to set ${settingKey} for guild ${guild.id} to ${jsonString}:`, error )
+          return host.respondTo( message, 'unexpected' )
+        }
       }
     } else {
       images = {}
@@ -143,11 +177,23 @@ class TwitchImageCommand extends Commando.Command
       }
     } else if ( args.url === 'clear' ) {
       delete images[args.username]
-      await backend.setGuildSetting( guild.id, settingKey, JSON.stringify( images ) )
+      const jsonString = JSON.stringify( images )
+      try {
+        await backend.setGuildSetting( guild.id, settingKey, jsonString )
+      } catch ( error ) {
+        log( `Failed to set ${settingKey} setting for guild ${guild.id} to ${jsonString}:`, error )
+        return host.respondTo( message, 'unexpected' )
+      }
       return host.respondTo( message, 'twitchimage_clear', args.username )
     } else {
       images[args.username] = args.url
-      await backend.setGuildSetting( guild.id, settingKey, JSON.stringify( images ) )
+      const jsonString = JSON.stringify( images )
+      try {
+        await backend.setGuildSetting( guild.id, settingKey, jsonString )
+      } catch ( error ) {
+        log( `Failed to set ${settingKey} setting for guild ${guild.id} to ${jsonString}:`, error )
+        return host.respondTo( message, 'unexpected' )
+      }
       return host.respondTo( message, 'twitchimage_set', args.username, args.url )
     }
   }
@@ -186,22 +232,38 @@ class TwitchFollowCommand extends Commando.Command
     const host = this._service.getHost()
     const backend = host.getBackend()
 
-    const guild = await backend.getGuildBySnowflake( message.guild.id )
+    let guild
+    try {
+      guild = await backend.getGuildBySnowflake( message.guild.id )
+    } catch ( error ) {
+      log( `Failed to fetch guild ${message.guild.id}:`, error )
+      return host.respondTo( message, 'unexpected' )
+    }
+
+    let setting
+    try {
+      setting = await backend.getGuildSetting( guild.id, settingKey )
+    } catch ( error ) {
+      log( `Failed to fetch ${settingKey} setting for guild ${guild.id}:`, error )
+      setting = null
+    }
 
     let subs: string[]
-    let setting = await backend.getGuildSetting( guild.id, settingKey )
-
     if ( setting && setting.value ) {
       try {
         subs = JSON.parse( setting.value )
-        if ( !Array.isArray( subs ) ) {
-          const message = `${settingKey} must be a JSON array.`
-          log( message )
-          throw new Error( message )
-        }
-      } catch ( error ) {
+        if ( !Array.isArray( subs ) )
+          throw new Error( `${settingKey} must be a JSON array.` )
+      } catch ( _ ) {
+        log( `The ${settingKey} setting of guild ${guild.id} was not a JSON array.` )
         subs = []
-        await backend.setGuildSetting( guild.id, settingKey, JSON.stringify( subs ) )
+        const jsonString = JSON.stringify( subs )
+        try {
+          await backend.setGuildSetting( guild.id, settingKey, jsonString )
+        } catch ( error ) {
+          log( `Failed to set ${settingKey} setting for guild ${guild.id} to ${jsonString}:`, error )
+          return host.respondTo( message, 'unexpected' )
+        }
       }
     } else {
       subs = []
@@ -228,13 +290,19 @@ class TwitchFollowCommand extends Commando.Command
         return host.respondTo( message, 'twitchfollow_nonexistent', username )
     }
 
+    subs.push( username )
+    const jsonString = JSON.stringify( subs )
+    try {
+      await backend.setGuildSetting( guild.id, settingKey, jsonString )
+    } catch ( error ) {
+      log( `Failed to set ${settingKey} setting for guild ${guild.id} to ${jsonString}:`, error )
+      return host.respondTo( message, 'unexpected' )
+    }
+
     if ( guilds )
       guilds.push( guild.id )
     else
       this._service.guildsFollowing.set( username, [guild.id] )
-
-    subs.push( username )
-    await backend.setGuildSetting( guild.id, settingKey, JSON.stringify( subs ) )
     return host.respondTo( message, 'twitchfollow_success', username )
   }
 }
@@ -269,25 +337,69 @@ null>
     const host = this._service.getHost()
     const backend = host.getBackend()
 
-    const guild = await backend.getGuildBySnowflake( message.guild.id )
-    const username = args.username
+    let guild: any // TODO: how do I get the model type
+    try {
+      guild = await backend.getGuildBySnowflake( message.guild.id )
+    } catch ( error ) {
+      log( `Failed to fetch guild ${message.guild.id}:`, error )
+      return host.respondTo( message, 'unexpected' )
+    }
 
-    const subsSetting = await backend.getGuildSetting( guild.id, settingKey )
-    if ( !subsSetting || !subsSetting.value )
+    let setting
+    try {
+      setting = await backend.getGuildSetting( guild.id, settingKey )
+    } catch ( error ) {
+      log( `Failed to fetch ${settingKey} setting for guild ${guild.id}:`, error )
+      setting = null
+    }
+
+    const username = args.username
+    if ( !setting || !setting.value )
       return host.respondTo( message, 'twitchunfollow_not_following', username )
 
-    let subs = JSON.parse( subsSetting.value )
+    let subs: string[]
+    try {
+      subs = JSON.parse( setting.value )
+      if ( !Array.isArray( subs ) )
+        throw new Error( `The ${settingKey} setting must be a JSON array.` )
+    } catch ( _ ) {
+      log( `The ${settingKey} setting for guild ${guild.id} was not a JSON array.` )
+      subs = []
+      const jsonString = JSON.stringify( subs )
+      try {
+        await backend.setGuildSetting( guild.id, settingKey, jsonString )
+      } catch ( error ) {
+        log( `Failed to set ${settingKey} setting for guild ${guild.id} to ${jsonString}:`, error )
+        return host.respondTo( message, 'unexpected' )
+      }
+    }
+
     if ( !subs.includes( username ) )
       return host.respondTo( message, 'twitchunfollow_not_following', username )
 
-    subs = subs.filter( (x: string) => x !== username )
-    let guilds = this._service.guildsFollowing.get( username )
+    const guilds = this._service.guildsFollowing.get( username )
+    let newGuilds: any[] = []
     if ( guilds ) {
-      guilds = guilds.filter( ( x: number ) => x !== guild.id )
-      if ( !guilds.length )
-        await this._service.unsubscribe( username )
+      newGuilds = guilds.filter( ( x: number ) => x !== guild.id )
+      if ( !newGuilds.length ) {
+        try {
+          await this._service.unsubscribe( username )
+        } catch ( error ) {
+          log( `Failed to unsubscribe from Twitch user ${username}:`, error )
+          return host.respondTo( message, 'unexpected' )
+        }
+      }
     }
-    await backend.setGuildSetting( guild.id, settingKey, JSON.stringify( subs ) )
+
+    subs = subs.filter( (x: string) => x !== username )
+    const jsonString = JSON.stringify( subs )
+    try {
+      await backend.setGuildSetting( guild.id, settingKey, jsonString )
+    } catch ( error ) {
+      log( `Failed to set ${settingKey} setting for guild ${guild.id} to ${jsonString}:`, error )
+      return host.respondTo( message, 'unexpected' )
+    }
+    this._service.guildsFollowing.set( username, newGuilds )
     return host.respondTo( message, 'twitchunfollow_success', username )
   }
 }
@@ -299,10 +411,10 @@ export class TwitchModule extends ModuleBase
   authProvider: ClientCredentialsAuthProvider
   apiClient: ApiClient
   listener: WebHookListener
-  channels: Map<number, string>
-  guildsFollowing: Map<string, number[]>
-  webhookSubscriptions: Map<string, Subscription>
-  currentStates: Map<string, HelixStream | null>
+  channels: Map<number, string> = new Map()
+  guildsFollowing: Map<string, number[]> = new Map()
+  webhookSubscriptions: Map<string, Subscription> = new Map()
+  currentStates: Map<string, HelixStream | null> = new Map()
   settingKeys = {
     channel: 'TwitchChannel',
     images: 'TwitchImages',
@@ -327,33 +439,52 @@ export class TwitchModule extends ModuleBase
       ssl: this.config.ssl
     } ) )
 
-    this.channels = new Map()
-    this.currentStates = new Map()
-    this.guildsFollowing = new Map()
-    this.webhookSubscriptions = new Map()
-
     this.listener.listen().then( async () => {
-      await this._backend._models.Guild.findAll( { attributes: ['id'] } ).then( async ( guilds: any[] ) => {
+      await this._backend._models.Guild.findAll( { attributes: ['id'] } )
+      .then( async ( guilds: any[] ) => {
         for ( const guild of guilds ) {
-          const guildID = guild.id
-
-          const channelSetting = await this._backend.getGuildSetting( guildID, this.settingKeys.channel )
-          if ( channelSetting && channelSetting.value ) {
-            this.channels.set( guildID, channelSetting.value )
+          let channelSetting
+          try {
+            channelSetting = await this._backend.getGuildSetting( guild.id, this.settingKeys.channel )
+          } catch ( error ) {
+            log( `Failed to fetch ${this.settingKeys.channel} setting for guild ${guild.id}:`, error )
+            continue
           }
 
-          const subsSetting = await this._backend.getGuildSetting( guildID, this.settingKeys.subscriptions )
+          if ( channelSetting && channelSetting.value ) {
+            this.channels.set( guild.id, channelSetting.value )
+          }
+
+          let subsSetting
+          try {
+            subsSetting = await this._backend.getGuildSetting( guild.id, this.settingKeys.subscriptions )
+          } catch ( error ) {
+            log( `Failed to fetch ${this.settingKeys.subscriptions} setting for guild ${guild.id}:`, error )
+            continue
+          }
           if ( !subsSetting || !subsSetting.value )
             continue
-          const subs = JSON.parse( subsSetting.value )
+
+          let subs: string[]
+          try {
+            subs = JSON.parse( subsSetting.value )
+            if ( !Array.isArray ( subs ) )
+              throw new Error( `The ${this.settingKeys.subscriptions} setting must be a JSON array.` )
+          } catch ( error ) {
+            log( `The ${this.settingKeys.subscriptions} setting for guild ${guild.id} was not a JSON array.` )
+            continue
+          }
 
           subs.forEach( ( username: string ) => {
             if ( this.guildsFollowing.has( username ) )
-              ( this.guildsFollowing.get( username ) as number[] ).push( guildID )
+              ( this.guildsFollowing.get( username ) as number[] ).push( guild.id )
             else
-              this.guildsFollowing.set( username, [guildID] )
+              this.guildsFollowing.set( username, [guild.id] )
           } )
         }
+      } )
+      .catch( (error: any) => {
+        log( "Failed to fetch guilds' Twitch subscriptions:", error )
       } )
 
       setInterval( () => {
@@ -361,8 +492,13 @@ export class TwitchModule extends ModuleBase
         this.authProvider.refresh()
       }, 24 * 3600 * 1000 )
 
-      for ( const username of this.guildsFollowing.keys() )
-        this.subscribe( username )
+      const subPromises = [...this.guildsFollowing.keys()].map( this.subscribe )
+      Promise.allSettled( subPromises ).then( (outcomes: any[]) => {
+        for ( const outcome of outcomes ) {
+          if ( outcome.status === 'rejected' )
+            log( "A Twitch subscription failed:", outcome.reason )
+        }
+      } )
     } )
   }
 
@@ -404,8 +540,18 @@ export class TwitchModule extends ModuleBase
 
   async subscribe( username: string )
   {
-    const user = await this.apiClient.helix.users.getUserByName( username )
-    if ( !user || this.webhookSubscriptions.has( user.name ) )
+    let userOrNull: HelixUser | null
+    try {
+      userOrNull = await this.apiClient.helix.users.getUserByName( username )
+    } catch ( error ) {
+      log( `Failed to fetch data for Twitch user ${username}:`, error )
+      return
+    }
+
+    if ( !userOrNull )
+      return
+    const user = userOrNull
+    if ( this.webhookSubscriptions.has( user.name ) )
       return
 
     const subscription = await this.listener.subscribeToStreamChanges( user, async ( stream?: HelixStream ) => {
@@ -419,7 +565,13 @@ export class TwitchModule extends ModuleBase
           const channelSnowflake = this.channels.get( guildID )
           if ( !channelSnowflake )
             return
-          const channel = await this._client.channels.fetch( channelSnowflake )
+          let channel
+          try {
+            channel = await this._client.channels.fetch( channelSnowflake )
+          } catch ( error ) {
+            log( `Failed to fetch channel ${channelSnowflake}`, error )
+            return
+          }
           if ( !channel || channel.type !== 'text' )
             return
 
@@ -432,7 +584,12 @@ export class TwitchModule extends ModuleBase
             .setTimestamp( stream.startDate )
 
           let imageURL = null
-          const imagesSetting = await this._backend.getGuildSetting( guildID, this.settingKeys.images )
+          let imagesSetting
+          try {
+            imagesSetting = await this._backend.getGuildSetting( guildID, this.settingKeys.images )
+          } catch ( error ) {
+            log( `Failed to fetch ${this.settingKeys.images} setting for guild ${guildID}:`, error )
+          }
           if ( imagesSetting && imagesSetting.value ) {
             const images = JSON.parse( imagesSetting.value )
             imageURL = images[user.name] || null
@@ -443,21 +600,35 @@ export class TwitchModule extends ModuleBase
           else
             embed.setImage( stream.thumbnailUrl.replace( '{width}', '1920' ).replace( '{height}', '1080' ) )
 
-          const game = await stream.getGame()
+          let game
+          try {
+            game = await stream.getGame()
+          } catch ( error ) {
+            log( "Failed to fetch Twitch game:", error )
+          }
           if ( game )
             embed.setDescription( `Playing **${game.name}**` );
 
           ( channel as TextChannel ).send( embed ).catch( error => {
             if ( error.message !== 'Missing Permissions' )
-              throw error
+              log( `Failed to send Twitch notification to channel ${channelSnowflake}:`, error )
           } )
         }
       }
       this.currentStates.set( user.name, stream || null )
       debug(`[twitch] set current state for ${user.name} to`, stream || null)
     } )
+    .catch( error => {
+      log( `Failed to subscribe to Twitch stream changes for ${user.name}:`, error )
+    } )
+
     if ( subscription ) {
-      const stream = await this.apiClient.helix.streams.getStreamByUserId( user )
+      let stream
+      try {
+        stream = await this.apiClient.helix.streams.getStreamByUserId( user )
+      } catch ( error ) {
+        log( `Failed to fetch Twitch stream for ${user.name}:`, error )
+      }
       this.currentStates.set( user.name, stream || null )
       debug(`[twitch] set current state for ${user.name} to`, stream || null)
       this.webhookSubscriptions.set( user.name, subscription )
@@ -470,7 +641,11 @@ export class TwitchModule extends ModuleBase
     const subscription = this.webhookSubscriptions.get( username )
     if ( !subscription )
       return
-    await subscription.stop()
+    try {
+      await subscription.stop()
+    } catch ( error ) {
+      log( `Failed to stop Twitch subscription for ${username}:`, error )
+    }
     this.webhookSubscriptions.delete( username )
   }
 }
