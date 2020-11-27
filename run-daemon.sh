57eb73fdcfb81cc866f5ce0cad032087bb776b18
Jeremias Pettinen <hi@jeremi.as>
2020-11-27 17:27:22 +0000

    Allow running run scrips from any directory

--- run-daemon.sh
+++ run-daemon.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
+cd "$(dirname "$0")"
+
 pkill -u "$USER" node
-node built/index.js -c config.json >/dev/null 2>&1 &
+node built/index.js -c config.json >> nyabot.log 2>&1 &
--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
+cd "$(dirname "$0")"
+
 pkill -u "$USER" node
 node built/index.js -c config.json
